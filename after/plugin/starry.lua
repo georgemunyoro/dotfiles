86c6280294a432723393d5e4d8bd0a030f4757b3
George Munyoro <george@munyoro.com>
2023-04-20 15:10:30 +0200

    add starry theme plugin

---
+++ after/plugin/starry.lua
@@ -0,0 +1,7 @@
+local starry = require("starry")
+
+starry.setup({
+	starry_deep_black = true,
+})
+
+vim.cmd("colorscheme moonlight")
--- lua/munyoro/packer.lua
+++ lua/munyoro/packer.lua
@@ -4,105 +4,107 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-  -- Packer can manage itself
-  use("wbthomason/packer.nvim")
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  use({ "nvim-telescope/telescope.nvim", tag = "0.1.1", requires = { { "nvim-lua/plenary.nvim" } } })
+	use({ "nvim-telescope/telescope.nvim", tag = "0.1.1", requires = { { "nvim-lua/plenary.nvim" } } })
 
-  use({
-    "rose-pine/neovim",
-    as = "rose-pine",
-    config = function()
-      vim.cmd("colorscheme rose-pine")
-    end,
-  })
-  use("dracula/vim")
+	-- colorschemes
+	-- use({
+	--   "rose-pine/neovim",
+	--   as = "rose-pine",
+	--   config = function()
+	--     vim.cmd("colorscheme rose-pine")
+	--   end,
+	-- })
+	-- use("dracula/vim")
+	use("ray-x/starry.nvim")
 
-  use({
-    "projekt0n/github-nvim-theme",
-    tag = "v0.0.7",
-    -- or                            branch = '0.0.x'
-    config = function()
-      require("github-theme").setup({
-        -- ...
-      })
+	use({
+		"projekt0n/github-nvim-theme",
+		tag = "v0.0.7",
+		-- or                            branch = '0.0.x'
+		config = function()
+			require("github-theme").setup({
+				-- ...
+			})
 
-      vim.cmd("colorscheme github_dark")
-    end,
-  })
+			vim.cmd("colorscheme github_dark")
+		end,
+	})
 
-  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-  use("nvim-treesitter/playground")
-  use("theprimeagen/harpoon")
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
 
-  use({
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    requires = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" }, -- Required
-      {
-        -- Optional
-        "williamboman/mason.nvim",
-        run = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-      },
-      { "williamboman/mason-lspconfig.nvim" }, -- Optional
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{
+				-- Optional
+				"williamboman/mason.nvim",
+				run = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" },  -- Required
-      { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      { "L3MON4D3/LuaSnip" },  -- Required
-    },
-  })
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	})
 
-  use("jose-elias-alvarez/null-ls.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
 
-  -- nvimtree
-  use("nvim-tree/nvim-tree.lua")
-  use("nvim-tree/nvim-web-devicons")
+	-- nvimtree
+	use("nvim-tree/nvim-tree.lua")
+	use("nvim-tree/nvim-web-devicons")
 
-  -- git
-  use("lewis6991/gitsigns.nvim")
-  use("dinhhuy258/git.nvim")
+	-- git
+	use("lewis6991/gitsigns.nvim")
+	use("dinhhuy258/git.nvim")
 
-  -- snippet engine
-  use("L3MON4D3/LuaSnip")
+	-- snippet engine
+	use("L3MON4D3/LuaSnip")
 
-  -- copilot
-  use("github/copilot.vim")
+	-- copilot
+	use("github/copilot.vim")
 
-  -- autoclose pairs
-  use("m4xshen/autoclose.nvim")
+	-- autoclose pairs
+	use("m4xshen/autoclose.nvim")
 
-  -- indent lines
-  use("lukas-reineke/indent-blankline.nvim")
+	-- indent lines
+	use("lukas-reineke/indent-blankline.nvim")
 
-  -- status line
-  use({
-    "nvim-lualine/lualine.nvim",
-    requires = { "nvim-tree/nvim-web-devicons", opt = true },
-  })
+	-- status line
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
 
-  -- notifications
-  use("rcarriga/nvim-notify")
+	-- notifications
+	use("rcarriga/nvim-notify")
 
-  -- github pr integration
-  use({
-    "pwntester/octo.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      "kyazdani42/nvim-web-devicons",
-    },
-    config = function()
-      require("octo").setup()
-    end,
-  })
+	-- github pr integration
+	use({
+		"pwntester/octo.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+			"kyazdani42/nvim-web-devicons",
+		},
+		config = function()
+			require("octo").setup()
+		end,
+	})
 
-  -- git worktrees
-  use("ThePrimeagen/git-worktree.nvim")
+	-- git worktrees
+	use("ThePrimeagen/git-worktree.nvim")
 end)
